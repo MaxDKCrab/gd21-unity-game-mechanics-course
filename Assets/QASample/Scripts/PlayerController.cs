edfb32340ff61b7c3ecd49ea0b61cc08966fe7b8
Max Deurell Kent <max.deurell@gmail.com>
2022-01-25 13:02:36 +0100

    Made player controller

--- Assets/QASample/Scripts/PlayerController.cs
+++ Assets/QASample/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Rigidbody myRb;
+    public float moveSpeed = 5f;
+    public float jumpForce = 500f;
+    
     void Update()
     {
+        var moveInput = Input.GetAxis("Horizontal");
         
+        myRb.velocity = new Vector3(moveSpeed * moveInput, myRb.velocity.y, 0);
+
+        //var jumpInput = Input.GetKeyDown(KeyCode.Space);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRb.AddForce(0, jumpForce, 0);
+        }
     }
 }
